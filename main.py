b1242a4a5ea403958fccf44a9e9f877b807f33ce
allybaubooa <allyhusseinbaubooa@gmail.com>
2022-10-07 19:31:15 +0400

    Bug Fixes and Some Enhancement in Text Inputs

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import mysql.connector
 from kivy.animation import Animation
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
 from kivy.utils import rgba
 from kivymd.app import MDApp
 from kivy.lang import Builder
@@ -21,7 +20,9 @@ from kivymd.uix.button import MDIconButton
 from kivymd.uix.imagelist import MDSmartTile
 from kivy.uix.image import AsyncImage
 from kivymd.uix.floatlayout import MDFloatLayout
-from kivymd.uix.behaviors import FakeRectangularElevationBehavior
+from kivymd.uix.behaviors import FakeRectangularElevationBehavior, HoverBehavior
+from kivymd.uix.label import MDLabel
+from kivymd.uix.textfield import MDTextFieldRect
 from twilio.rest import Client
 
 Window.size = (390, 700)
@@ -29,6 +30,10 @@ Window.size = (390, 700)
 
 # Load class
 class LoaderImage(AsyncImage):
+    pass
+
+
+class HoverLabel(HoverBehavior, MDLabel, ButtonBehavior):
     pass
 
 
@@ -52,7 +57,7 @@ class But_pressed(MDIconButton):
     pass
 
 
-class InputField(TextInput):
+class InputField(MDTextFieldRect):
     def __init__(self, **kwargs):
         super().__init__(multiline=False, input_filter="int", **kwargs)
 
@@ -76,10 +81,27 @@ class OGWallpaperApp(MDApp):
     cursor = database.cursor()
     id = 1
 
+    data = {
+        'Contact Us': 'email',
+        'Sign Out': 'logout'
+    }
+
     def build(self):
         screen = Builder.load_file("wallpaper.kv")
         self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "BlueGray"
         return screen
+
+    def callback(self, ins):
+        if ins.icon == "logout":
+            self.root.ids.speed_dial.close_stack()
+            self.sign_out()
+            toast("You have been signed out!!!", background=[41 / 255, 76 / 255, 96 / 255, 0.8])
+        elif ins.icon == "email":
+            self.root.ids.speed_dial.close_stack()
+            self.root.ids.screen_manager.transition.direction = "left"
+            self.root.ids.screen_manager.current = "contactus"
+            self.clear_result()
 
     # Dark/Light mode Switch class
     def check(self, checkbox, value):
@@ -121,8 +143,8 @@ class OGWallpaperApp(MDApp):
             for image in range(50):
                 result = ImageButton(source=f"https://source.unsplash.com/random/180x240/?{search_image}={image}",
                                      pos_hint={"center_x": .5}, size_hint=(None, None), size=("180dp", "240dp"))
-                print(result.source)
                 self.root.ids.search_list.add_widget(result)
+                result.bind(on_release=lambda x: self.select_image(x.source))
 
     # Categories Goto
     def goto(self, source):
@@ -172,34 +194,25 @@ class OGWallpaperApp(MDApp):
                                   pos_hint={"center_x": .5},
                                   size_hint=(None, None),
                                   size=("240dp", "350dp"))
-            results2 = MDIconButton(id=f"bt{images}",
-                                    icon="heart-outline",
-                                    theme_icon_color="Custom",
-                                    icon_color=(1, 0, 0, 1),
-                                    pos_hint={"center_x": .5, "center_y": .5})
             results3 = MDIconButton(id=f"dw{images}",
                                     icon="download",
                                     theme_icon_color="Custom",
                                     icon_color=(0, 0, 0, 1),
                                     pos_hint={"center_x": .5, "center_y": .5})
-            results2.bind(on_release=lambda x=results2: self.pressed(results.id))
-            results3.bind(on_release=lambda x=results3: self.download(results.id))
+            results3.bind(on_release=lambda y: self.download(y))
             self.root.ids.recent_list.add_widget(results)
-            self.root.ids.recent_list.add_widget(results2)
             self.root.ids.recent_list.add_widget(results3)
-
-    def pressed(self, img_url):
-        print(img_url)
 
     # Download Image
     def download(self, img_url):
-        api_call = f"https://picsum.photos/id/{img_url}/800/1200"
+        image_id = img_url.id.replace("dw", "")
+        api_call = f"https://picsum.photos/id/{image_id}/800/1200"
         print(api_call)
-        request.urlretrieve(api_call, f"downloads/{img_url}.png")
+        request.urlretrieve(api_call, f"downloads/{image_id}.png")
 
     # NavBar Color Return Button
     def return_but(self):
-        for i in range(1, 4):
+        for i in range(1, 3):
             self.root.ids[f"nav{i + 1}"].text_color = rgba(173, 182, 196, 255)
         self.root.ids.nav1.text_color = rgba(253, 275, 277, 255)
 
@@ -207,7 +220,7 @@ class OGWallpaperApp(MDApp):
     def changenav_color(self, ins):
         if ins in self.root.ids.values():
             current_id = list(self.root.ids.keys())[list(self.root.ids.values()).index(ins)]
-            for i in range(4):
+            for i in range(3):
                 if f"nav{i + 1}" == current_id:
                     self.root.ids[f"nav{i + 1}"].text_color = rgba(253, 275, 277, 255)
                 else:
@@ -270,10 +283,11 @@ class OGWallpaperApp(MDApp):
     # Download in Select Page
     def download_sel(self):
         api_call = self.sel
-        request.urlretrieve(api_call, f"downloads/new.png")
+        img_name = f"new{random.randint(1, 10000)}"
+        request.urlretrieve(api_call, f"downloads/{img_name}.png")
 
     # Register Login
-    def send_data(self, email, password, f_name, l_name):
+    def send_data(self, email, password, f_name, l_name, cc, phone_no):
         if re.fullmatch(self.regex, email.text) and email.text != "" and password.text != "" and \
                 f_name.text != "" and l_name.text != "":
 
@@ -291,33 +305,37 @@ class OGWallpaperApp(MDApp):
                                     f" '{l_name.text}')")
                 self.database.commit()
                 toast("OTP has been sent on your phone!!", background=[41 / 255, 76 / 255, 96 / 255, 0.8])
-                self.otp_verification(f_name)
-                self.receive_data(email, password, id=1)
+                self.otp_verification(email, password, f_name, cc, phone_no)
                 email.text = ""
                 password.text = ""
                 f_name.text = ""
                 l_name.text = ""
-        elif email.text == "" or password.text == "" or f_name.text == "" or l_name.text == "":
+        elif email.text == "" or password.text == "" or f_name.text == "" \
+                or l_name.text == "" or cc.text == "" or cc.phone_no == "":
             toast("Please Enter All the Details to Proceed!", background=[0.8, 0, 0, 1])
         else:
             toast("Invalid Details", background=[0.8, 0, 0, 1])
 
     # OTP Verification
-    def otp_verification(self, f_name):
+    def otp_verification(self, otpemail, otppassword, f_name, otpcc, otpphone):
+        self.user_email = otpemail.text
+        self.user_pw = otppassword.text
         self.first_name = f_name.text
         self.root.ids.screen_manager.current = "otp"
         self.otp = random.randint(1000, 9999)
         account_sid = ACCOUNT_SID
         auth_token = AUTH_TOKEN
+        self.phone_numer = f"{otpcc.text}{otpphone.text}"
+        print(self.phone_numer)
 
         client = Client(account_sid, auth_token)
 
         msg = client.messages.create(
             body=f"Your OTP Password is: {self.otp}",
             from_="+19259408526",
-            to="+23058163182"
+            to=self.phone_numer
         )
-        return self.first_name
+        return self.user_email, self.user_pw, self.first_name
 
     # OTP Check
     def check_otp(self, otpInput):
@@ -332,13 +350,13 @@ class OGWallpaperApp(MDApp):
         self.otp = random.randint(1000, 9999)
         account_sid = ACCOUNT_SID
         auth_token = AUTH_TOKEN
-
+        print(self.phone_numer)
         client = Client(account_sid, auth_token)
 
         msg = client.messages.create(
             body=f"Your OTP Password is: {self.otp}",
             from_="+19259408526",
-            to="+23058163182"
+            to=self.phone_numer
         )
         toast("OTP has been sent again!!", background=[41 / 255, 76 / 255, 96 / 255, 0.8])
 
@@ -353,6 +371,8 @@ class OGWallpaperApp(MDApp):
             for j in self.cursor:
                 if password.text == j[0]:
                     if id == 0:
+                        email.text = ""
+                        password.text = ""
                         self.root.ids.screen_manager.current = "home"
                         toast("You have successfully login your account!!",
                               background=[41 / 255, 76 / 255, 96 / 255, 0.8])
@@ -360,6 +380,13 @@ class OGWallpaperApp(MDApp):
                     toast("Incorrect Password!! Try Again.", background=[0.8, 0, 0, 1])
         elif email.text == "" or password.text == "":
             toast("Please Enter Details to Login!!!", background=[0.8, 0, 0, 1])
+        else:
+            toast("Please Enter Proper Login Details!!!", background=[0.8, 0, 0, 1])
+
+    # Sign Out
+    def sign_out(self):
+        self.root.ids.screen_manager.transition.direction = "right"
+        self.root.ids.screen_manager.current = "login"
 
     # Contact Us email
     def send_email(self, name, email_address, message):
@@ -367,7 +394,8 @@ class OGWallpaperApp(MDApp):
         msg["Subject"] = "New User Message!"
         msg["To"] = "allyhb98@gmail.com"
         msg["From"] = formataddr((name.text, email_address.text))
-        msg.set_content(f"Hi Team,\n\nMy Name is {name.text}.\n\n{message.text}\n\nRegards,\n{name.text}")
+        msg.set_content(f"Hi Team,\n\nMy Name is {name.text}.\n\nMy email address is: {email_address.text}."
+                        f"\n\n{message.text}\n\nRegards,\n{name.text}")
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
             smtp.login(EMAIL, PASSWORD)
             smtp.send_message(msg)
